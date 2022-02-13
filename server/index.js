866caf66514503a5a5453c8b2cad600e9bcdbaf4
Brian <a92021122@gmail.com>
2022-02-14 02:22:31 +0800

    feat: 新增auth route

--- server/index.js
+++ server/index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-
 dotenv.config();
+const authRoute = require("./routes").auth;
 
 // connect to DB
 mongoose
@@ -18,8 +18,10 @@ mongoose
     console.log(err);
   });
 
+// middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("api/user", authRoute);
 
 app.listen(8080, () => {
   console.log("Server running on port 8080.");
---
+++ server/routes/auth.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+
+router.use((req, res, next) => {
+  console.log("A request is coming in to auth.js");
+  next();
+});
+
+router.get("/testAPI", (req, res) => {
+  const msgObj = {
+    message: "Test API is working.",
+  };
+  return res.json(msgObj);
+});
+
+module.exports = router;
